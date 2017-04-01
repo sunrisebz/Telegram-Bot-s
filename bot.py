3c26fe79f34b3bb5fd727bbd447551cb183de45e
Роман <aintcor@gmail.com>
2017-04-01 21:38:21 +0900

    New version vk_auth

--- bot.py
+++ bot.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
-import smtplib
-import grab
-import lxml.html as html
 import vk_api
-
-
-from email.mime.text import MIMEText
 
 
 bot = telebot.TeleBot(config.token)
@@ -28,17 +22,17 @@ def vk_comment(message):
         code = input('Input secret code')
         return code, False
 
-    if login and password != ' ':
-        vk_session = vk_api.VkApi(login, password, auth_handler=two_factor_handler)
-        try:
-            vk_session.authorization()
-        except vk_api.AuthorizationError as error_msg:
-            print(error_msg)
-            return
-        vk = vk_session.get_api()
-        response = vk.board.addTopic(group_id=127727264, title='New Title', tezt='hello')  # Используем метод wall.get
-        if response['items']:
-             print(response['items'][0])
+    vk_session = vk_api.VkApi(login, password, auth_handler=two_factor_handler)
+    try:
+        vk_session.auth()
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+
+    vk = vk_session.get_api()
+    response = vk.board.addTopic(group_id=127727264, title='New Title', tezt='hello')  # Используем метод wall.get
+    if response['items']:
+        print(response['items'][0])
 
 
 if __name__ == "__main__":
